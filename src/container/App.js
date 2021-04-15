1704b6625033ed4e800ad95f5d530d7898c3901b
Pedro Pacheco <pacheckdev@gmail.com>
2021-04-15 02:57:19 -0300

    Added Node-Sass to project

--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
+import HomePage from '../HomePage';
+
 function App() {
   return (
-    <div >
-      hello, world!
-      testando
+    <div>
+      <HomePage />
     </div>
   );
 }
