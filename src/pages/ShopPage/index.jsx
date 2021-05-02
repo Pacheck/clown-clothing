77ae237fb020350c771a70e76bfe1055e7cf6256
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-02 15:35:42 -0300

    added shop page data

---
+++ src/pages/ShopPage/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../../components/CollectionPreview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div className="shops-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
