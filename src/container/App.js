f0a22eef23d6bba8a206dafee42e378fcb0c351b
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-02 15:36:13 -0300

    added padding, created hatpage

--- src/container/App.js
+++ src/container/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
+import ShopPage from '../pages/ShopPage';
+// import HatPage from '../pages/HatPage';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop" component={ShopPage}/>
+      {/* <Route path="/shop/hats" component={HatPage}/> */}
     </Switch>
   );
 }
---
+++ src/pages/HatPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HatPage = () => {
+  return <div>HatPage</div>;
+};
+
+export default HatPage;
