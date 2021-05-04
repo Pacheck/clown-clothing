b153e485ffc7bdc4a0810e5222f381488a9eb90e
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-04 02:58:53 -0300

    adding firebase auth, changed from function componento to class based

--- src/container/App.js
+++ src/container/App.js
@@ -1,19 +1,47 @@
-import './App.css';
+import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
+import { auth } from '../firebase/firebase.utils';
+
+import './App.css';
+
+// import HatPage from '../pages/HatPage';
+import SignPage from '../pages/SignPage';
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
-// import HatPage from '../pages/HatPage';
+import Header from '../components/Header';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/shop" component={ShopPage}/>
-      {/* <Route path="/shop/hats" component={HatPage}/> */}
-    </Switch>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/shop" component={ShopPage}/>
+          <Route exact path="/signin" component={SignPage}/>
+          {/* <Route path="/shop/hats" component={HatPage}/> */}
+        </Switch>
+      </>
+    );
+  }
+  
 }
 
 export default App;
