24b94a8dc06d11d20680d1c39bef9a6a605db1ec
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-04 03:00:26 -0300

    added sign up page

---
+++ src/pages/SignPage/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignIn from "../../components/SignIn";
+
+import "./index.scss";
+
+const SignPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignPage;
