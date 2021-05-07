5f932125743a54945d50c8ab140d3a8c783e7744
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-07 02:43:16 -0300

    added new case add item into reducer

--- src/redux/reducer/cart.reducer.js
+++ src/redux/reducer/cart.reducer.js
@@ -1,17 +1,25 @@
 import { CartActionTypes } from '../actions/types.actions';
 
+import { addItemToCart } from '../../utils/Cart';
+
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    const { TOGGLE_CART_HIDDEN } = CartActionTypes;
+    const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
     
     switch (action.type) {
         case TOGGLE_CART_HIDDEN: 
             return {
                 ...state, 
                 hidden: !state.hidden
+            }
+        case ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
