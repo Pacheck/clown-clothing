7d06c2cdcfe4555809e3caaa100c55915f10b21c
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-07 00:17:24 -0300

    added constant type to actions

--- src/redux/actions/user.actions.js
+++ src/redux/actions/user.actions.js
@@ -1,4 +1,7 @@
+import { UserActionTypes } from '../actions/types.actions';
+const { SET_CURRENT_USER } = UserActionTypes;
+
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: SET_CURRENT_USER,
     payload: user,
 })
