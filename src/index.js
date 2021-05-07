3f809c011a85031ba5e39805d97875424e308afe
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-06 23:58:01 -0300

    added provider around app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
+import { Provider } from 'react-redux';
+
+import store from './redux/store/redux.store';
 import App from './container/App';
 
+import './index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
