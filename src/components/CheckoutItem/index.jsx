db8740975c712ebc73d26b0e5dc7a0417b2d03e8
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-09 00:50:17 -0300

    added handlers to quantity state change

--- src/components/CheckoutItem/index.jsx
+++ src/components/CheckoutItem/index.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/actions/cart.actions";
+
 import "./index.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  const handleRemoveItem = () => clearItem(cartItem);
+
+  const handleIncreaseQuantity = () => addItem(cartItem);
+
+  const handleDecreaseQuantity = () => removeItem(cartItem);
+
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
-      <span className="price">{price}</span>
-      <span className="remove-button">&#10005;</span>
+      <span className="quantity">
+        <div className="arrow" onClick={handleDecreaseQuantity}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={handleIncreaseQuantity}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">${price}</span>
+      <span className="remove-button" onClick={handleRemoveItem}>
+        &#10005;
+      </span>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
