27e4d298509ed99c77977bb82f41339c1069dbf1
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-08 23:47:20 -0300

    created selectors to handle with re-renders

--- src/redux/selectors/cart.js
+++ src/redux/selectors/cart.js
@@ -7,7 +7,17 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce((acumulator, currentItem) => acumulator + currentItem.quantity, 0)
+)
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((acumulator, currentItem) => acumulator + currentItem.quantity * currentItem.price, 0)
 )
