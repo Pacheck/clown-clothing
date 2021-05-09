a216e379926d53638d012dfda71ecfa125463f3d
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-09 00:50:53 -0300

    added new case remove item and clear item

--- src/redux/reducer/cart.reducer.js
+++ src/redux/reducer/cart.reducer.js
@@ -1,6 +1,6 @@
 import { CartActionTypes } from '../actions/types.actions';
 
-import { addItemToCart } from '../../utils/Cart';
+import { addItemToCart, removeItemFromCart } from '../../utils/Cart';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
+    const { TOGGLE_CART_HIDDEN, ADD_ITEM, REMOVE_ITEM, CLEAR_ITEM_FROM_CART } = CartActionTypes;
     
     switch (action.type) {
         case TOGGLE_CART_HIDDEN: 
@@ -20,6 +20,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+        case REMOVE_ITEM:
+            return {
+                ...state, 
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CLEAR_ITEM_FROM_CART: 
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
         default:
             return state;
