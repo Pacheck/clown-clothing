59281b26d47622c1d8f8c2d4fbc285d3a2d487d2
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-09 00:50:35 -0300

    added types and actions to remove/add item

--- src/redux/actions/cart.actions.js
+++ src/redux/actions/cart.actions.js
@@ -1,12 +1,22 @@
 import { CartActionTypes } from './types.actions';
 
-const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
+const { TOGGLE_CART_HIDDEN, ADD_ITEM, REMOVE_ITEM, CLEAR_ITEM_FROM_CART } = CartActionTypes;
 
 export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN
-})
+});
 
 export const addItem = (item) => ({
     type: ADD_ITEM,
+    payload: item
+});
+
+export const removeItem = (item) => ({
+    type: REMOVE_ITEM,
+    payload: item,
+})
+
+export const clearItemFromCart = (item) => ({
+    type: CLEAR_ITEM_FROM_CART,
     payload: item
 })
