29e3accea7c330c69af9189c9ad00ad466b67526
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-11 23:35:23 -0300

    inserted directory and shop reducer to root

--- src/redux/reducer/root.reducer.js
+++ src/redux/reducer/root.reducer.js
@@ -4,6 +4,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user.reducer';
 import cartReducer from './cart.reducer';
+import directoryReducer from './directory.reducer';
+import shopReducer from './shop.reducer';
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +15,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
