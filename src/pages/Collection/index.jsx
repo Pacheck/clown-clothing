9610cdfa78c8d5ddc116874c123a3a90bc1f69ce
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-13 13:21:41 -0300

    added mappedc collection item

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -9,9 +9,15 @@ import { selectCollection } from "../../redux/selectors/shop";
 import "./index.scss";
 
 const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>CATEGORY PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
