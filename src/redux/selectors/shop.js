5eba7f671fc58984a2fe0defc5ef476adee1e33f
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-13 13:22:53 -0300

    removed collection id map, created new selector preview

--- src/redux/selectors/shop.js
+++ src/redux/selectors/shop.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5,
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = memoize(createSelector(
@@ -16,7 +8,12 @@ export const selectCollections = memoize(createSelector(
     shop => shop.collections
 ));
 
+export const selectCollectionForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections.find( collection => collection.id  === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
 )
